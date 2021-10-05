ce5e302ad0d146720ab1756a24f4d705be52053e
KricsDavid <kdodi05@gmail.com>
2021-10-05 21:45:21 +0200

    adad

--- alma.py
+++ alma.py
@@ -1,6 +1,3 @@
-character_name ="Jani"
-character_age = "35"
-print ( character_name +  " egy valodi ember")
-print ( character_age + " evet elt a kis kukimanoka")
-print ("Szeretett kukimanokent elni")
+phrase = "Giraffe Academy"
+print (phrase + " is cool")
 

662c31f2342fb3c2933dc53abb9a666edf704f20
KricsDavid <kdodi05@gmail.com>
2021-10-05 21:36:20 +0200

    ad

--- alma.py
+++ alma.py
@@ -1,6 +1,6 @@
 character_name ="Jani"
 character_age = "35"
-print ("character_name"  "egy valodi ember")
-print ("35 evet elt a kis kukimanoka")
+print ( character_name +  " egy valodi ember")
+print ( character_age + " evet elt a kis kukimanoka")
 print ("Szeretett kukimanokent elni")
 
